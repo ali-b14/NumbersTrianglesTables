ce61f947942a79de505543d8babd808f7a935f9e
ali-b14 <alib714@gmail.com>
2024-03-05 17:59:09 -0500

    Completeed the small and large triangles

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,19 +3,27 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
+        String n = "";
+        int =
+
         return null;
     }
 
     public static String getRow(int numberOfStars) {
-        
+        StringBuilder sb = new StringBuilder();
+
+        int width = 20;
+        String outcome = Triangles(width)
         return ;
     }
 
     public static String getSmallTriangle() {
-        return null;
+
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+
+        return getTriangle(10);
     }
 }
